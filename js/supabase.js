8be533a90fe326b63778ecea7d4e683534dc515a
Maxwell Osman Kamara <102341373+iamdontolo@users.noreply.github.com>
2026-08-27 13:26:20 +0000

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://supabase.com/dashboard/project/ziatsxqcpbjkqqhkqfft";
+
+const SUPABASE_KEY = "sb_publishable_dcnulglg7IntduX5CSnRCw_DVLPP26h";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
